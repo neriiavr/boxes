cc4a0350a360132fa8b5e6773ca8f333fa0cd68a
neriiavr <avrupalineriman@gmail.com>
2022-11-28 16:02:13 +0100

    dark mode boxes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from "react"  
 import './App.css';
+import boxes from "./boxes"
+import Box from "./Box"
 
 function App() {
+  const [squares, setSquares] = React.useState(boxes)
+
+  function toggle(id) {
+    setSquares(prevSquares => {
+      return prevSquares.map((square) => {
+        return square.id === id ? {...square, on: !square.on} : square
+
+      })
+
+    })
+  }
+
+  const squareElements = squares.map(square => (
+    <Box 
+      key={square.id} 
+      id={square.id}
+      on={square.on}
+      toggle={toggle}
+      />
+  ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+      {squareElements}
+   </main>
+    
   );
 }
 
---
+++ src/Box.js
@@ -0,0 +1,20 @@
+
+import React, { useState } from "react" 
+
+export default function Box(props) {
+    const [on, setOn] = React.useState(props.on)
+  
+
+    const styles = {
+        backgroundColor: props.on ? "#222222" : "transparent" 
+    }
+
+    
+    return (
+        <div 
+        style={styles} 
+        className="box" 
+        onClick={()=>props.toggle(props.id)}>
+        </div>
+    )
+}
